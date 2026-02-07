1f254386213b3a3fc36a4834425df605fbd4bcf2
itsRaeff <mohamedraef.hosni@esprit.tn>
2026-02-07 19:05:55 +0100

    commit of workshop 2 (Liste des suggestions)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { ListSuggestionComponent } from './list-suggestion/list-suggestion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    FooterComponent,
+    ListSuggestionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [
     provideClientHydration()
